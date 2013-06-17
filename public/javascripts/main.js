5a7d154e56934d4d0f50623ccaf66781afe499ee
Stephen Timko <s.w.timko@gmail.com>
2013-06-16 23:37:23 -0400

    wip

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,9 @@
 $(function(){
-	console.log('hello');
+  var degree_offset = -40;
+  //var left_offset = 0;
+  $('#projects-list li').each(function(index, elem){
+    $(elem).transition({rotate: degree_offset + 'deg'})
+    degree_offset += 10;
+    //left_offset += 10;
+  });
 });
