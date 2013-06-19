48cb7bc6765c3fff03a3f4120948ba719acabac6
Stephen Timko <s.w.timko@gmail.com>
2013-06-18 23:28:56 -0400

    grid

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,5 @@
 $(function(){
-  var degree_offset = -40;
-  //var left_offset = 0;
-  $('#projects-list li').each(function(index, elem){
-    $(elem).transition({rotate: degree_offset + 'deg'})
-    degree_offset += 10;
-    //left_offset += 10;
-  });
+  //$('#projects-list li').each(function(index, elem){
+    //$(elem).transition({rotateY: '30deg'})
+  //});
 });
