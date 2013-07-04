9bf258ae8bcea9a08b6119f6ca343953b4cae477
Stephen Timko <s.w.timko@gmail.com>
2013-07-04 00:00:46 -0400

    back at it

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,9 @@
 $(function(){
   var degree_offset = -45,
       hover_card = false,
-      $projects = $('#projects-list li');
+      $projects = $('#projects-list li'),
+      current_timer,
+      initial_hover = true;
   
   //$projects.transition({opacity: .2});
   $projects.each(function(index, elem){
@@ -13,12 +15,25 @@ $(function(){
       $elem.css('z-index', 1)
       $elem.transition({opacity: 1});
       hover_card = true;
+      if(initial_hover){
+        initial_hover = false;
+        $projects.not($elem).transition({opacity: .2});
+      }
     });
     $elem.on('mouseleave', function(event){
       $elem = $(event.currentTarget);
       $elem.css('z-index', 0)
       $elem.transition({opacity: .2});
-      hover_card = false;    
+      hover_card = false;
+
+      clearTimeout(current_timer);
+      current_timer = setTimeout(function(){
+        current_timer = null;
+        if(!hover_card){
+          initial_hover = true;
+          $projects.transition({opacity:1});    
+        }
+      }, 500); 
     });
   });
 });
