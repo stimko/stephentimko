96ff04c8a6f3342169509787bc95acd3a269763c
Stephen Timko <s.w.timko@gmail.com>
2013-07-06 00:34:23 -0400

    card ui logic

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,7 +5,32 @@ $(function(){
       current_timer,
       initial_hover = true
       selected_card_degree = '',
-      selected_card = null;
+      $selected_card = null;
+
+  function animate_in_selected_card($card){
+    $selected_card = $card;
+    old_degrees = $selected_card.css('rotate');
+    $selected_card.transition({rotate:0}, function(){
+      $selected_card.transition({scale:1.5,rotateY:'-180deg'}, 1000, function(){
+        selected_card_degree = old_degrees;
+      });
+    });
+  }
+
+  function animate_out_selected_card($card){
+    if(!$card){
+      $card = $selected_card;
+    }
+    $selected_card = null;
+    $card.transition({scale:1,rotateY:'0deg'}, 1000, function(){
+      $card.css('z-index', 0)
+      $card.transition({rotate:selected_card_degree});
+      if($selected_card){
+        $card.trigger('mouseleave');
+      }    
+    }); 
+  }
+
   
   $projects.each(function(index, elem){
   	$elem = $(elem);
@@ -24,7 +49,7 @@ $(function(){
 
     $elem.on('mouseleave', function(event){
       $elem = $(event.currentTarget);
-      if(selected_card !== null && $elem[0] === selected_card[0]){
+      if($selected_card !== null && $elem[0] === $selected_card[0]){
         return;
       }
       $elem.css('z-index', 0)
@@ -34,29 +59,26 @@ $(function(){
       clearTimeout(current_timer);
       current_timer = setTimeout(function(){
         current_timer = null;
-        if(!hover_card && !selected_card){
+        if(!hover_card && !$selected_card){
           initial_hover = true;
           $projects.transition({opacity:1}, 300);    
         }
-      }, 500);
+      }, 750);
     });
 
     $elem.on('click', function(event){
       $elem = $(event.currentTarget);
-      selected_card = $elem;
-      selected_card_degree = $elem.css('rotate');
-      $elem.transition({rotate:0}, function(){
-        $(elem).transition({scale:1.5,rotateY:'-180deg'}, 1000);
-      });
+      if($selected_card){
+        animate_out_selected_card()
+        animate_in_selected_card($elem)
+      } else {
+        animate_in_selected_card($elem)  
+      }
+
     });
     $('.back i', $elem).on('click', function(event){
       event.stopImmediatePropagation();
-      selected_card = null;
-      $elem = $(event.currentTarget).closest('li')
-      $(elem).transition({scale:1,rotateY:'0deg'}, 1000, function(){
-        $elem.css('z-index', 0)
-        $elem.transition({rotate:selected_card_degree});    
-      }); 
+      animate_out_selected_card($(event.currentTarget).closest('li'))
     });
 
   });
